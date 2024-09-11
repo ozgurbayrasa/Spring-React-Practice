364e223157d8759b296576e446d2a658f8bc4dc4
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-09-12 00:26:53 +0300

    Adding Table with Bootstrap

--- React Practice/react-todo-demo/react-todos/src/App.js
+++ React Practice/react-todo-demo/react-todos/src/App.js
@@ -3,10 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          We made our first react application!
-        </p>
+    <div className='mt-5 container'>
+        <div className='card'>
+          <div className='card-header'>
+            Your Todo's
+          </div>
+
+          <div className='card-body'>
+            <table className='table table-hover'>
+              <thead>
+                <tr>
+                  <th scope='col'>#</th>
+                  <th scope='col'>Description</th>
+                  <th scope='col'>Assigned</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope='row'>1</th>
+                  <td>Feed Dog</td>
+                  <td>Eric</td>
+                </tr>
+                <tr>
+                  <th scope='row'>2</th>
+                  <td>Get Haircut</td>
+                  <td>Eric</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
     </div>
   );
 }
