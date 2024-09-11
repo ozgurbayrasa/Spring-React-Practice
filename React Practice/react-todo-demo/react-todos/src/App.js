3a541c085903a0d9f37a71db969bc2e78979c13e
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-09-12 00:08:01 +0300

    initial commit

---
+++ React Practice/react-todo-demo/react-todos/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          We made our first react application!
+        </p>
+    </div>
+  );
+}
+export default App;
