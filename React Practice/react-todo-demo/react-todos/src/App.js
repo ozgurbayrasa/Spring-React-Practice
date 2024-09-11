b3be168b2b2212160af8b2898a6096318143ec64
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-09-12 01:55:16 +0300

    Moving Table as Component

--- React Practice/react-todo-demo/react-todos/src/App.js
+++ React Practice/react-todo-demo/react-todos/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import ToDoRowItem from './components/TodoRowItem';
+import TodoTable from './components/TodoTable';
 
 function App() {
 
@@ -18,33 +17,7 @@ function App() {
           </div>
 
           <div className='card-body'>
-            <table className='table table-hover'>
-              <thead>
-                <tr>
-                  <th scope='col'>#</th>
-                  <th scope='col'>Description</th>
-                  <th scope='col'>Assigned</th>
-                </tr>
-              </thead>
-              <tbody>
-                <ToDoRowItem 
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}/>
-
-                <ToDoRowItem 
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}/>
-
-                <ToDoRowItem 
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}/>
-
-                
-              </tbody>
-            </table>
+            <TodoTable todos={todos}/>
           </div>
         </div>
     </div>
